3870c2747c82ce23adf930acaa3fdb12e330974b
Nabil KHALED <nabil.khaled@etudiant.univ-reims.fr>
2022-11-05 01:16:34 +0100

    Update Page 2

--- src/pages/test.js
+++ src/pages/test.js
@@ -5,7 +5,7 @@ export default ({ data }) => (
   <React.Fragment>
     <h2>{data.site.siteMetadata.title}</h2>
     <div>{data.site.siteMetadata.description}</div>
-    <Link to="/">Back to home</Link>
+    <Link to="/">Nabil KHALED</Link>
   </React.Fragment>
 )
 
